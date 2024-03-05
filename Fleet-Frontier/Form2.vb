8a800f29ad7d4bf73ff07ee01c72e2f766035a31
mwangi-hilda <mwangihw06@gmail.com>
2024-03-05 21:35:36 +0300

    rebuilt project

--- Fleet-Frontier/Form2.vb
+++ Fleet-Frontier/Form2.vb
@@ -3,7 +3,52 @@
         Me.WindowState = FormWindowState.Minimized
     End Sub
 
+    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UsernameTextBox.Text = "Username"
+        UsernameTextBox.ForeColor = Color.Black
+
+        TextBox1.Text = "Password"
+        TextBox1.ForeColor = Color.Black
+    End Sub
+
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim newForm As New Form1()
+
+        newForm.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub UsernameTextBox_GotFocus(sender As Object, e As EventArgs) Handles UsernameTextBox.GotFocus
+        If UsernameTextBox.Text = "Username" Then
+            UsernameTextBox.Text = ""
+            UsernameTextBox.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub UsernameTextBox_LostFocus(sender As Object, e As EventArgs) Handles UsernameTextBox.LostFocus
+        If UsernameTextBox.Text = "" Then
+            UsernameTextBox.Text = "Username"
+            UsernameTextBox.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBox1_GotFocus(sender As Object, e As EventArgs) Handles TextBox1.GotFocus
+        If TextBox1.Text = "Password" Then
+            TextBox1.Text = ""
+            TextBox1.PasswordChar = "•"
+            TextBox1.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub TextBox1_LostFocus(sender As Object, e As EventArgs) Handles TextBox1.LostFocus
+        If TextBox1.Text = "" Then
+            TextBox1.Text = "Password"
+            TextBox1.PasswordChar = ""
+            TextBox1.ForeColor = Color.Black
+        End If
     End Sub
 End Class
