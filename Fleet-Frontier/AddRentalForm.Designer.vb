b29578307ab10051648b02ad157c320497ba1806
ngure1 <ngugijoseph700@gmail.com>
2024-03-30 12:45:20 +0300

    Add create and read rental

--- Fleet-Frontier/AddRentalForm.Designer.vb
+++ Fleet-Frontier/AddRentalForm.Designer.vb
@@ -28,15 +28,13 @@ Partial Class AddRentalForm
         ExitAddRentalButton = New Button()
         AddRentalLabel = New Label()
         Panel2 = New Panel()
-        ReturnDateLabel = New Label()
-        ReturnDateTimePicker = New DateTimePicker()
+        VehicleComboBox = New ComboBox()
+        Label1 = New Label()
+        CustomerPhoneNumberTextBox = New TextBox()
         ClearRentalButton = New Button()
         AddRentalButton = New Button()
         CancelRentalButton = New Button()
-        RentingDateTimePicker = New DateTimePicker()
-        Label5 = New Label()
         VehicleLabel = New Label()
-        VehicleTextBox = New TextBox()
         CustomerNameLabel = New Label()
         CustomerNameTextBox = New TextBox()
         Panel1.SuspendLayout()
@@ -52,9 +50,9 @@ Partial Class AddRentalForm
         Panel1.Controls.Add(AddRentalLabel)
         Panel1.Dock = DockStyle.Top
         Panel1.Location = New Point(0, 0)
-        Panel1.Margin = New Padding(6)
+        Panel1.Margin = New Padding(4)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(965, 90)
+        Panel1.Size = New Size(568, 55)
         Panel1.TabIndex = 2
         ' 
         ' PictureBox1
@@ -62,9 +60,9 @@ Partial Class AddRentalForm
         PictureBox1.Dock = DockStyle.Left
         PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
         PictureBox1.Location = New Point(0, 0)
-        PictureBox1.Margin = New Padding(6)
+        PictureBox1.Margin = New Padding(4)
         PictureBox1.Name = "PictureBox1"
-        PictureBox1.Size = New Size(91, 90)
+        PictureBox1.Size = New Size(54, 55)
         PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
         PictureBox1.TabIndex = 6
         PictureBox1.TabStop = False
@@ -78,10 +76,10 @@ Partial Class AddRentalForm
         ExitAddRentalButton.FlatStyle = FlatStyle.Flat
         ExitAddRentalButton.Font = New Font("Leelawadee UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         ExitAddRentalButton.ForeColor = Color.Black
-        ExitAddRentalButton.Location = New Point(903, 0)
-        ExitAddRentalButton.Margin = New Padding(6)
+        ExitAddRentalButton.Location = New Point(532, 0)
+        ExitAddRentalButton.Margin = New Padding(4)
         ExitAddRentalButton.Name = "ExitAddRentalButton"
-        ExitAddRentalButton.Size = New Size(62, 90)
+        ExitAddRentalButton.Size = New Size(36, 55)
         ExitAddRentalButton.TabIndex = 5
         ExitAddRentalButton.Text = "X"
         ExitAddRentalButton.UseVisualStyleBackColor = True
@@ -90,10 +88,10 @@ Partial Class AddRentalForm
         ' 
         AddRentalLabel.AutoSize = True
         AddRentalLabel.Font = New Font("Gabriola", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        AddRentalLabel.Location = New Point(100, 8)
-        AddRentalLabel.Margin = New Padding(6, 0, 6, 0)
+        AddRentalLabel.Location = New Point(59, 5)
+        AddRentalLabel.Margin = New Padding(4, 0, 4, 0)
         AddRentalLabel.Name = "AddRentalLabel"
-        AddRentalLabel.Size = New Size(183, 74)
+        AddRentalLabel.Size = New Size(114, 45)
         AddRentalLabel.TabIndex = 0
         AddRentalLabel.Text = "Add Rental"
         AddRentalLabel.TextAlign = ContentAlignment.TopCenter
@@ -102,40 +100,48 @@ Partial Class AddRentalForm
         ' 
         Panel2.BackColor = Color.Silver
         Panel2.BorderStyle = BorderStyle.FixedSingle
-        Panel2.Controls.Add(ReturnDateLabel)
-        Panel2.Controls.Add(ReturnDateTimePicker)
+        Panel2.Controls.Add(VehicleComboBox)
+        Panel2.Controls.Add(Label1)
+        Panel2.Controls.Add(CustomerPhoneNumberTextBox)
         Panel2.Controls.Add(ClearRentalButton)
         Panel2.Controls.Add(AddRentalButton)
         Panel2.Controls.Add(CancelRentalButton)
-        Panel2.Controls.Add(RentingDateTimePicker)
-        Panel2.Controls.Add(Label5)
         Panel2.Controls.Add(VehicleLabel)
-        Panel2.Controls.Add(VehicleTextBox)
         Panel2.Controls.Add(CustomerNameLabel)
         Panel2.Controls.Add(CustomerNameTextBox)
-        Panel2.Location = New Point(23, 131)
-        Panel2.Margin = New Padding(6)
+        Panel2.Location = New Point(14, 80)
+        Panel2.Margin = New Padding(4)
         Panel2.Name = "Panel2"
-        Panel2.Size = New Size(914, 1076)
+        Panel2.Size = New Size(538, 657)
         Panel2.TabIndex = 3
         ' 
-        ' ReturnDateLabel
+        ' VehicleComboBox
         ' 
-        ReturnDateLabel.AutoSize = True
-        ReturnDateLabel.Font = New Font("Gabriola", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        ReturnDateLabel.Location = New Point(28, 823)
-        ReturnDateLabel.Margin = New Padding(6, 0, 6, 0)
-        ReturnDateLabel.Name = "ReturnDateLabel"
-        ReturnDateLabel.Size = New Size(164, 63)
-        ReturnDateLabel.TabIndex = 13
-        ReturnDateLabel.Text = "Return Date"
+        VehicleComboBox.FormattingEnabled = True
+        VehicleComboBox.Location = New Point(30, 305)
+        VehicleComboBox.Margin = New Padding(1, 2, 1, 2)
+        VehicleComboBox.Name = "VehicleComboBox"
+        VehicleComboBox.Size = New Size(498, 33)
+        VehicleComboBox.TabIndex = 16
         ' 
-        ' ReturnDateTimePicker
+        ' Label1
         ' 
-        ReturnDateTimePicker.Location = New Point(202, 839)
-        ReturnDateTimePicker.Name = "ReturnDateTimePicker"
-        ReturnDateTimePicker.Size = New Size(527, 47)
-        ReturnDateTimePicker.TabIndex = 12
+        Label1.AutoSize = True
+        Label1.Font = New Font("Gabriola", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(24, 161)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(201, 39)
+        Label1.TabIndex = 15
+        Label1.Text = "Customer Phone Number"
+        ' 
+        ' CustomerPhoneNumberTextBox
+        ' 
+        CustomerPhoneNumberTextBox.Location = New Point(30, 204)
+        CustomerPhoneNumberTextBox.Margin = New Padding(4)
+        CustomerPhoneNumberTextBox.Name = "CustomerPhoneNumberTextBox"
+        CustomerPhoneNumberTextBox.Size = New Size(498, 31)
+        CustomerPhoneNumberTextBox.TabIndex = 14
         ' 
         ' ClearRentalButton
         ' 
@@ -145,10 +151,10 @@ Partial Class AddRentalForm
         ClearRentalButton.FlatStyle = FlatStyle.Flat
         ClearRentalButton.Font = New Font("Gabriola", 9F, FontStyle.Bold)
         ClearRentalButton.ForeColor = Color.Blue
-        ClearRentalButton.Location = New Point(38, 910)
-        ClearRentalButton.Margin = New Padding(6, 8, 6, 8)
+        ClearRentalButton.Location = New Point(22, 555)
+        ClearRentalButton.Margin = New Padding(4, 5, 4, 5)
         ClearRentalButton.Name = "ClearRentalButton"
-        ClearRentalButton.Size = New Size(208, 72)
+        ClearRentalButton.Size = New Size(122, 44)
         ClearRentalButton.TabIndex = 11
         ClearRentalButton.Text = "Clear"
         ClearRentalButton.UseVisualStyleBackColor = False
@@ -162,10 +168,10 @@ Partial Class AddRentalForm
         AddRentalButton.FlatStyle = FlatStyle.Flat
         AddRentalButton.Font = New Font("Gabriola", 9F, FontStyle.Bold)
         AddRentalButton.ForeColor = Color.Black
-        AddRentalButton.Location = New Point(669, 910)
-        AddRentalButton.Margin = New Padding(6)
+        AddRentalButton.Location = New Point(394, 555)
+        AddRentalButton.Margin = New Padding(4)
         AddRentalButton.Name = "AddRentalButton"
-        AddRentalButton.Size = New Size(208, 72)
+        AddRentalButton.Size = New Size(122, 44)
         AddRentalButton.TabIndex = 10
         AddRentalButton.Text = "Add"
         AddRentalButton.UseVisualStyleBackColor = False
@@ -179,79 +185,53 @@ Partial Class AddRentalForm
         CancelRentalButton.FlatStyle = FlatStyle.Flat
         CancelRentalButton.Font = New Font("Gabriola", 9F, FontStyle.Bold)
         CancelRentalButton.ForeColor = Color.Red
-        CancelRentalButton.Location = New Point(351, 910)
-        CancelRentalButton.Margin = New Padding(6)
+        CancelRentalButton.Location = New Point(206, 555)
+        CancelRentalButton.Margin = New Padding(4)
         CancelRentalButton.Name = "CancelRentalButton"
-        CancelRentalButton.Size = New Size(208, 72)
+        CancelRentalButton.Size = New Size(122, 44)
         CancelRentalButton.TabIndex = 9
         CancelRentalButton.Text = "Cancel"
         CancelRentalButton.UseVisualStyleBackColor = False
-        ' 
-        ' RentingDateTimePicker
-        ' 
-        RentingDateTimePicker.Location = New Point(202, 756)
-        RentingDateTimePicker.Margin = New Padding(6)
-        RentingDateTimePicker.Name = "RentingDateTimePicker"
-        RentingDateTimePicker.Size = New Size(527, 47)
-        RentingDateTimePicker.TabIndex = 8
-        ' 
-        ' Label5
-        ' 
-        Label5.AutoSize = True
-        Label5.Font = New Font("Gabriola", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label5.Location = New Point(28, 740)
-        Label5.Margin = New Padding(6, 0, 6, 0)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(177, 63)
-        Label5.TabIndex = 7
-        Label5.Text = "Renting Date"
         ' 
         ' VehicleLabel
         ' 
         VehicleLabel.AutoSize = True
         VehicleLabel.Font = New Font("Gabriola", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        VehicleLabel.Location = New Point(19, 494)
-        VehicleLabel.Margin = New Padding(6, 0, 6, 0)
+        VehicleLabel.Location = New Point(30, 264)
+        VehicleLabel.Margin = New Padding(4, 0, 4, 0)
         VehicleLabel.Name = "VehicleLabel"
-        VehicleLabel.Size = New Size(106, 63)
+        VehicleLabel.Size = New Size(68, 39)
         VehicleLabel.TabIndex = 3
         VehicleLabel.Text = "Vehicle"
-        ' 
-        ' VehicleTextBox
-        ' 
-        VehicleTextBox.Location = New Point(34, 564)
-        VehicleTextBox.Margin = New Padding(6)
-        VehicleTextBox.Name = "VehicleTextBox"
-        VehicleTextBox.Size = New Size(843, 47)
-        VehicleTextBox.TabIndex = 2
         ' 
         ' CustomerNameLabel
         ' 
         CustomerNameLabel.AutoSize = True
         CustomerNameLabel.Font = New Font("Gabriola", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        CustomerNameLabel.Location = New Point(28, 217)
-        CustomerNameLabel.Margin = New Padding(6, 0, 6, 0)
+        CustomerNameLabel.Location = New Point(24, 50)
+        CustomerNameLabel.Margin = New Padding(4, 0, 4, 0)
         CustomerNameLabel.Name = "CustomerNameLabel"
-        CustomerNameLabel.Size = New Size(208, 63)
+        CustomerNameLabel.Size = New Size(134, 39)
         CustomerNameLabel.TabIndex = 1
         CustomerNameLabel.Text = "Customer Name"
         ' 
         ' CustomerNameTextBox
         ' 
-        CustomerNameTextBox.Location = New Point(38, 287)
-        CustomerNameTextBox.Margin = New Padding(6)
+        CustomerNameTextBox.Location = New Point(30, 93)
+        CustomerNameTextBox.Margin = New Padding(4)
         CustomerNameTextBox.Name = "CustomerNameTextBox"
-        CustomerNameTextBox.Size = New Size(843, 47)
+        CustomerNameTextBox.Size = New Size(498, 31)
         CustomerNameTextBox.TabIndex = 0
         ' 
         ' AddRentalForm
         ' 
-        AutoScaleDimensions = New SizeF(17F, 41F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(965, 1222)
+        ClientSize = New Size(568, 674)
         Controls.Add(Panel1)
         Controls.Add(Panel2)
         FormBorderStyle = FormBorderStyle.None
+        Margin = New Padding(2)
         Name = "AddRentalForm"
         Text = "AddRentalForm"
         Panel1.ResumeLayout(False)
@@ -270,12 +250,10 @@ Partial Class AddRentalForm
     Friend WithEvents ClearRentalButton As Button
     Friend WithEvents AddRentalButton As Button
     Friend WithEvents CancelRentalButton As Button
-    Friend WithEvents RentingDateTimePicker As DateTimePicker
-    Friend WithEvents Label5 As Label
     Friend WithEvents VehicleLabel As Label
-    Friend WithEvents VehicleTextBox As TextBox
     Friend WithEvents CustomerNameLabel As Label
     Friend WithEvents CustomerNameTextBox As TextBox
-    Friend WithEvents ReturnDateLabel As Label
-    Friend WithEvents ReturnDateTimePicker As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CustomerPhoneNumberTextBox As TextBox
+    Friend WithEvents VehicleComboBox As ComboBox
 End Class
--- Fleet-Frontier/AddRentalForm.vb
+++ Fleet-Frontier/AddRentalForm.vb
@@ -1,9 +1,15 @@
-﻿Public Class AddRentalForm
+﻿Imports MySql.Data.MySqlClient
+
+Public Class AddRentalForm
+    Dim connection As MySqlConnection
+    Dim command As MySqlCommand
+    Dim ConnectionString As String = "server=localhost;port=33062;userid=root;password='default123';database=fleet_finder"
+    Dim adapter As MySqlDataAdapter
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearRentalButton.Click
         CustomerNameTextBox.Text = ""
-        VehicleTextBox.Text = ""
-
+        CustomerPhoneNumberTextBox.Text = ""
+        VehicleComboBox.Text = ""
     End Sub
     Private Sub CancelRentalButton_Click(sender As Object, e As EventArgs) Handles CancelRentalButton.Click
         Me.Close()
@@ -11,5 +17,70 @@
 
     Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles ExitAddRentalButton.Click
         Me.Close()
+    End Sub
+
+
+    Private Sub PopulateVehicleComboBox()
+        ' Fetch data for vehicles
+        Dim vehicleQuery As String = "SELECT * FROM vehicle WHERE is_available > 0"
+        adapter = New MySqlDataAdapter(vehicleQuery, connection)
+        Dim vehicleTable As New DataTable()
+        adapter.Fill(vehicleTable)
+
+        ' Populate combo box with vehicle data
+        For Each row As DataRow In vehicleTable.Rows
+            Dim vehicleId As Integer = Convert.ToInt32(row("vehicle_id"))
+            Dim numberPlate As String = row("numberPlate").ToString()
+            Dim displayName As String = vehicleId.ToString() & " - " & numberPlate
+            VehicleComboBox.Items.Add(displayName)
+        Next
+    End Sub
+    Private Sub AddRentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Connect to your database
+        connection = New MySqlConnection(ConnectionString)
+        Try
+            connection.Open()
+            PopulateVehicleComboBox()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    Private Sub AddRentalButton_Click(sender As Object, e As EventArgs) Handles AddRentalButton.Click
+        connection = New MySqlConnection(ConnectionString)
+        Try
+            Dim customerName As String = CustomerNameTextBox.Text
+            Dim customerPhoneNumber As String = CustomerPhoneNumberTextBox.Text
+            Dim vehicleId As Integer = Integer.Parse(VehicleComboBox.SelectedItem.ToString().Split("-"c)(0).Trim())
+
+
+            ' Construct the SQL query with placeholders
+            Dim createRentalQuery As String = "INSERT INTO hire (vehicle_id, customer_name, customer_phone_number, is_returned) VALUES (@vehicleId, @customerName, @customerPhoneNumber, FALSE);"
+
+            ' Create a MySqlConnection and MySqlCommand
+            Using connection As New MySqlConnection(ConnectionString)
+                Using command As New MySqlCommand(createRentalQuery, connection)
+                    ' Add parameters
+                    command.Parameters.AddWithValue("@vehicleId", vehicleId)
+                    command.Parameters.AddWithValue("@customerName", customerName)
+                    command.Parameters.AddWithValue("@customerPhoneNumber", customerPhoneNumber)
+
+                    ' Open the connection
+                    connection.Open()
+
+                    ' Execute the query
+                    command.ExecuteNonQuery()
+
+                    ' Inform the user about the successful operation
+                    MessageBox.Show("Rental added successfully.")
+                End Using
+            End Using
+
+        Catch ex As Exception
+            ' Handle any exceptions
+            MessageBox.Show($"Error adding rental: {ex.Message}")
+        End Try
     End Sub
 End Class
